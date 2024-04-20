5a8ea55fb2ad09cff039df6f83ef1bc97a7fb42c
Krish Shah <2003kshah@gmail.com>
2024-04-20 14:22:28 -0400

    updated server

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe("esp8266/health")  # Make sure this is the correct topic
+    print("Connected with result code "+str(rc))
 
-def on_message(client, userdata, msg):
-    print(f"Message received: {msg.topic} {msg.payload.decode()}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    client.subscribe("/esp8266/health")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+def on_message(client, userdata, message):
+    print("Received message '" + str(message.payload.decode()) + "' on topic '" + message.topic)
+    
 
-try:
-    client.connect("128.197.55.102", 1883, 60)  # Connect to the broker
-    client.loop_forever()  # Process network traffic and dispatch callbacks
-except Exception as e:
-    print(f"Error connecting to MQTT broker: {e}")
+def main():
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "client_id")
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect('168.122.159.92', 1883) 
+    # Connect to the MQTT server and process messages in a background thread. 
+    mqtt_client.loop_start() 
+
+if __name__ == '__main__':
+    print('MQTT to InfluxDB bridge')
+    main()
+
